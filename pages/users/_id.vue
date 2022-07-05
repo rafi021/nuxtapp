b68fdbd68eb32fb56ab2b5c644e529d38cac6e24
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-05 18:41:36 +0600

    Nuxt Validate Method

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -3,3 +3,11 @@
   <h1>Helllo User Id: {{ $route.params.id }}</h1>
 </div>
 </template>
+
+<script>
+export default{
+  validate(context){
+    return /^\d+$/.test(context.params.id)
+  }
+}
+</script>
