3f25dbd920fcbd05eef374782037e4e05530a421
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-05 18:37:10 +0600

    load user on route parameter

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,24 @@
 <template>
-<h1>Hello User Page</h1>
+<div class="">
+  <h1>Hello User Page</h1>
+
+  <input type="text" v-model="id">
+  <button @click="loadUser">Load User</button>
+
+</div>
 </template>
+
+<script>
+export default{
+  data() {
+      return {
+        id: ''
+      }
+  },
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.id}`)
+    }
+  }
+}
+</script>
