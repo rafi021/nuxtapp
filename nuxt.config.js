6808f06586d16fb17cf921622c50fbf2d7dc4897
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-07 12:01:21 +0600

    create navigation

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' },
     ],
-    scripts: [
+    script: [
       {
         src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',
         type:'text/javascript'
--- pages/users.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div class="bg-danger">
-  <h4>You can see me in all users related pages</h4>
-  <nuxt-child />
-</div>
-</template>
-
-<script>
-</script>
