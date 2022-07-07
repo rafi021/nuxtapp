1297ff67f8e6887b52161685930a3d5dcdebbbb6
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-07 12:31:09 +0600

    using nuxt-link

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -8,10 +8,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
+          <nuxt-link class="nav-link active" aria-current="page" to="/">Home</nuxt-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="users">Users</a>
+          <nuxt-link class="nav-link" to="users">Users</nuxt-link>
         </li>
       </ul>
 
