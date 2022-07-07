79dabc2de7908e63677b1d5869e3e0c097443cb1
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-07 12:28:51 +0600

    layouts and error page

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nav-bar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'blog'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script>
-export default {
+export default{
   name: 'IndexPage',
+  layout: 'blog'
 }
 </script>
