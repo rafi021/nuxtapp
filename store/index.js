2e979bf5ecf2f3e15bc2d8520897854ef05d0f92
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-09 02:48:58 +0600

    create vuex store

---
+++ store/index.js
@@ -0,0 +1,27 @@
+// create a store
+export const state =  () => ({
+  posts: {}
+});
+
+
+//getters
+export const getters = {
+  posts(state){
+    return state.posts
+  }
+}
+
+//mutations
+export const mutations = {
+  SET_POSTS(state, posts) {
+    state.posts = posts
+  }
+}
+
+
+// actions
+export const actions = {
+  setPosts({commit}, posts){
+    commit("SET_POSTS", posts);
+  }
+}
