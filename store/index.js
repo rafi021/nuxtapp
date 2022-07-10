cfef58c929b7f0759f47e36fec3e0994496a2d61
rafi021 <mahmud.ibrahim021@gmail.com>
2022-07-10 22:53:15 +0600

    post spa mode

--- store/index.js
+++ store/index.js
@@ -23,12 +23,12 @@ export const mutations = {
 
 // actions
 export const actions = {
-  async nuxtServerInit({commit}){
-    let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    commit("SET_POSTS", data);
-  }
-
-  // setPosts({commit}, posts){
-  //   commit("SET_POSTS", posts);
+  // async nuxtServerInit({commit}){
+  //   let {data} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  //   commit("SET_POSTS", data);
   // }
+
+  setPosts({commit}, posts){
+    commit("SET_POSTS", posts);
+  }
 }
